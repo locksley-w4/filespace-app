4e19e8774c97a179d81b513a8051149cf5ff3675
locksley <84179770+locksley-w4@users.noreply.github.com>
2025-11-01 20:55:22 +0500

    Getting ready for production

--- server/src/config/sessionConfig.js
+++ server/src/config/sessionConfig.js
@@ -1,19 +1,19 @@
-import session from 'express-session'
-import dotenv from 'dotenv'
+// import session from 'express-session'
+// import dotenv from 'dotenv'
 
-dotenv.config();
+// dotenv.config();
 
-if (!process.env.SESSION_SECRET) {
-    throw new Error('Environment variable SESSION_SECRET must be set')
-}
+// if (!process.env.SESSION_SECRET) {
+//     throw new Error('Environment variable SESSION_SECRET must be set')
+// }
 
-export const sessionConfig = session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        httpOnly: true,
-        maxAge: 1000 * 3600 * 24,
-        secure: false
-    }
-});
+// export const sessionConfig = session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//         httpOnly: true,
+//         maxAge: 1000 * 3600 * 24,
+//         secure: false
+//     }
+// });
--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,44 @@
 import express from "express";
 import { userRouter } from "./routes/userRoutes.js";
-import { sessionConfig } from "./config/sessionConfig.js";
+// import { sessionConfig } from "./config/sessionConfig.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { handleError } from "./services/utils.js";
+import path from "path";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.use(cors({
+const __filename = import.meta.url;
+const __dirname = path.dirname(__filename);
+
+app.use(
+  cors({
     origin: "http://localhost:3000",
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(express.json());
 
-app.use(sessionConfig);
+// app.use(sessionConfig);
 
 app.use("/api/v1", userRouter);
 
-app.use(handleError)
+if (process.env.NODE_ENV === "production") {
+  const clientDir = path.resolve("../../client", "build");
+  console.log(clientDir);
+  
+  app.use(express.static(clientDir));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve(clientDir, "index.html"));
+  });
+}
+
+app.use(handleError);
 
 app.listen(port, (err) => {
   if (err) console.error(err);
